4f7ef91adebcfea6737ae482a730a45217472b74
xyz0z0 <xyz0z0.xyz@gmail.com>
2020-07-17 10:20:16 +0800

    Commit

--- src/com/company/leetcode35/Solution.java
+++ src/com/company/leetcode35/Solution.java
@@ -5,7 +5,24 @@ class Solution {
 
     public static void main(String[] args) {
         int[] array = new int[]{1, 3, 3, 4, 4, 5};
-        System.out.println(searchInsert(array, 1));
+        System.out.println(search(array, 1));
+    }
+
+    public static int search(int[] nums, int target) {
+
+        int left = 0;
+        int right = nums.length - 1;
+        int ans = nums.length;
+        while (left <= right) {
+            int mid = ((right - left) / 2) + left;
+            if (target <= nums[mid]) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
     }
 
 //    public static int searchInsert(int[] nums, int target) {
